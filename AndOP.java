918f41ff58a3e1ad972626d6b2bc28437f925eaf
Wayneiiibb104 <33171923+Wayneiiibb104@users.noreply.github.com>
2017-11-17 15:29:51 +0800

    Add files via upload

---
+++ AndOP.java
@@ -0,0 +1,8 @@
+class AndOP{
+	public static void main(String[] args){
+		int money = 30000; //現金
+		int day = 7; //假期天數
+		System.out.print("澳洲團成行：");
+		System.out.println(money>=40000 && day>=7);
+	}
+}
