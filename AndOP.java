f3eafb363968121c91bd2420e7295ab9d07f9223
Wayneiiibb104 <33171923+Wayneiiibb104@users.noreply.github.com>
2017-11-17 15:30:11 +0800

    Delete AndOP.java

--- AndOP.java
+++
@@ -1,8 +0,0 @@
-class AndOP{
-	public static void main(String[] args){
-		int money = 30000; //現金
-		int day = 7; //假期天數
-		System.out.print("澳洲團成行：");
-		System.out.println(money>=40000 && day>=7);
-	}
-}
